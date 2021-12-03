3b6c5197f4f18478142016a4a7421828de09ab33
alexxxesss <alexeenkoalexxx@yandex.ru>
2021-12-03 23:14:22 +0300

    home 01.12

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,3 +1,14 @@
+import json
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    list_2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+    filename_1 = "input_1.json"
+    filename_2 = "input_2.json"
+
+    with open(filename_1, 'x') as f_1:
+        json_data = json.dumps(list_1, indent=4)
+        
+    # with open(filename_2, 'x') as f_2:
+    #     f_2.write(list_2)
