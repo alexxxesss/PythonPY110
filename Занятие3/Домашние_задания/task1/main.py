1442a5db2ddfc820c5954b1123e25269a027a012
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2021-12-04 00:23:31 +0300

    ok.home

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -9,16 +9,12 @@ if __name__ == "__main__":
     filename_3 = "output.json"
 
     with open(filename_1, 'w') as f_1:
-        json_data_1 = json.dumps(list_1)
-        f_1.write(json_data_1)
-        print(json.dumps(list_1))
+        f_1.write(json.dumps(list_1))
         
     with open(filename_2, 'w') as f_2:
-        json_data_2 = json.dumps(list_2)
-        f_2.write(json_data_2)
+        f_2.write(json.dumps(list_2))
 
     with open(filename_1, 'r') as file_1:
         with open(filename_2, 'r') as file_2:
             with open(filename_3, 'w') as file_3:
-                json_data_3 = json.dumps(json.load(file_1) + json.load(file_2))
-                file_3.write(json_data_3)
+                file_3.write(json.dumps(json.load(file_1) + json.load(file_2)))
